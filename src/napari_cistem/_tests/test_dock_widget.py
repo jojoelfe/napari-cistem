661d146311b722d146541e1dd11eea4db18c5a19
Johannes Elferich <johannes.elferich@umassmed.edu>
2021-11-07 15:57:12 -0500

    Fixed test

--- src/napari_cistem/_tests/test_dock_widget.py
+++ src/napari_cistem/_tests/test_dock_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "napari-cistem"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
+MY_WIDGET_NAMES = ["cisTEM"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
